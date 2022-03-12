9eb3cdcddee9c647bafd046d868ee553d9a279d4
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-03-12 18:18:01 +0200

    events

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 console.log('activating javascript')
 
+
+//   create an array and add albums
 // when you click on an album add class selected
 // which is a css that adds some background
+document.addEventListener('click', function handleClick(event) {
+    event.target.classList.add('selected');
+  });
+  
+// disable click
+document.addEventListener('onclick', function drop_selected(event) {
+    event.target.classList.remove('selected');
+});
+  
